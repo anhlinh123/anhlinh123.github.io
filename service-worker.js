93e251d82837e5bc45e300968955ea858f8c5362
Nguyen Anh Linh <linh.nguyen@motional.com>
2023-04-01 00:36:08 +0800

    play sound

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
 self.addEventListener('push', function(event) {
-    const promiseChain = self.registration.showNotification(event.data.text());
-    const audio = new Audio('sound.wav')
-    audio.addEventListener("canplaythrough", (event) => {
-        audio.play();
+    const promiseChain = self.registration.showNotification(event.data.text(), {
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        requireInteraction: true,
+        sound: "sound.wav",
+        silent: false
     });
 
     event.waitUntil(promiseChain);
