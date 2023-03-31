a85f1e6028ef7414833bf5c4a9ec3d8bc67ec3aa
Nguyen Anh Linh <linh.nguyen@motional.com>
2023-04-01 00:10:21 +0800

    add sound

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 self.addEventListener('push', function(event) {
     const promiseChain = self.registration.showNotification(event.data.text());
+    const audio = new Audio('sound.wav')
+    audio.addEventListener("canplaythrough", (event) => {
+        audio.play();
+    });
 
     event.waitUntil(promiseChain);
 })
